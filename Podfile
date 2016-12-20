1130a9f95bb88bff2810915c7ae1df0bcfbc45c6
pz1943 <pz1943@163.com>
2016-12-20 23:50:42 +0800

    for mothers.

---
+++ Podfile
@@ -0,0 +1,5 @@
+platform :ios, '8.0'
+use_frameworks!
+target ‘BabyClock' do
+pod 'SQLite.swift', '~> 0.11.1'
+end
---
+++ Pods/Target Support Files/Pods-BabyClock/Pods-BabyClock-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_BabyClockVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_BabyClockVersionString[];
+
